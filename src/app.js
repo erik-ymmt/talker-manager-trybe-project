304a9ef282f3bd7f788a51b0459ccb0b2c431399
Erik Yamamoto <erik.ymmt@gmail.com>
2022-10-04 16:12:41 -0300

    add requirement 2

--- src/app.js
+++ src/app.js
@@ -14,4 +14,19 @@ app.get('/talker', async (_req, res) => {
   }
 });
 
+app.get('/talker/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const talkers = await readFile();
+    const talkerById = talkers.find((talker) => Number(talker.id) === Number(id));
+    if (!talkerById) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+    res.status(200).json(talkerById);
+  } catch (error) {
+    res.status(400).json(error);
+    console.log(error);
+  }
+});
+
 module.exports = app;
